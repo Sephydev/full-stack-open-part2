530efd07c745d14fae7ba6425e42a56e2540bcef
Sephydev <nicolas.bussiere@outlook.fr>
2025-03-31 15:02:23 +0200

    Modify data type of course & refactor code

--- course-information/src/App.jsx
+++ course-information/src/App.jsx
@@ -1,4 +1,13 @@
-const Header = (props) => <h1>{props.course}</h1>
+const Course = (props) => {
+  return (
+    <>
+      <Header course={props.course} />
+      <Content parts={props.course.parts} />
+    </>
+  )
+}
+
+const Header = (props) => <h1>{props.course.name}</h1>
 
 const Content = (props) => (
   <div>
@@ -18,36 +27,28 @@ const Total = (props) => <p>Number of exercises {props.talk}</p>
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamental of React',
         exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
         exercises: 14,
-      },
-    ],
+        id: 3
+      }
+    ]
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises
-        }
-      />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
