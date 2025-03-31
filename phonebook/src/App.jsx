2466049826ffc36ff8ebabf84f8c72c64e7ea515
Sephydev <nicolas.bussiere@outlook.fr>
2025-03-31 18:33:38 +0200

    Add form functionality

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -4,21 +4,38 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ])
+
   const [newName, setNewName] = useState('')
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <div>debug: {newName}</div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        {
+          persons.map((person) => <li key={person.name}>{person.name}</li>)
+        }
+      </ul>
     </div>
   )
 }
