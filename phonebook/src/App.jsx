fdc0239a58446a7cc6d0e3aa3f463930a651ccf6
Sephydev <nicolas.bussiere@outlook.fr>
2025-04-01 11:00:34 +0200

    Add input for number

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,34 +2,55 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'May Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName }
+
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+
     if (persons.findIndex((person) => person.name === newPerson.name) !== -1) {
-      console.log(newPerson.name)
       alert(`${newPerson.name} is already added to the phonebook.`)
     } else {
       setPersons(persons.concat(newPerson))
     }
+
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
+      <div>debug: {newName} / {newNumber}</div>
+      <label htmlFor='search'>Filter shown with </label>
+      <input id='search' />
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleNewName} />
+          <label htmlFor='name'>Name:</label>
+          <input id='name' value={newName} onChange={handleNewName} />
+          <br />
+          <label htmlFor='name'>Number:</label>
+          <input id='name' value={newNumber} onChange={handleNewNumber} />
         </div>
         <div>
           <button type="submit">Add</button>
@@ -38,7 +59,10 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {
-          persons.map((person) => <li key={person.name}>{person.name}</li>)
+          persons.map((person) =>
+            <li key={person.id}>
+              {person.name} {person.number}
+            </li>)
         }
       </ul>
     </div>
