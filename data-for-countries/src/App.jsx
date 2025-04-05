860b67dabe32b423d9940dc8780fe86d2b73ca05
Sephydev <nicolas.bussiere@outlook.fr>
2025-04-05 14:41:12 +0200

    Implement search function

--- data-for-countries/src/App.jsx
+++ data-for-countries/src/App.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import Countries from './components/Countries'
+
 const App = () => {
+  const [search, setSearch] = useState('')
+  const [countries, setCountries] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => setCountries(response.data))
+  }, [])
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
+  }
+
+  if (!countries) {
+    return null
+  }
+
   return (
-    <p>Hello, World!</p>
+    <>
+      Find countries <input value={search} onChange={handleChange} />
+      <Countries countries={countries} search={search} />
+    </>
   )
 }
 
---
+++ data-for-countries/src/components/Countries.jsx
@@ -0,0 +1,46 @@
+const Countries = ({ countries, search }) => {
+
+  const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(search))
+
+  if (filteredCountries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  }
+
+  if (filteredCountries.length <= 10 && filteredCountries.length > 1) {
+    return (
+      <ul>
+        {filteredCountries.map(country =>
+          <li key={country.name.common}>{country.name.common}</li>
+        )}
+      </ul>
+    )
+  }
+
+  if (filteredCountries.length === 1) {
+    const country = filteredCountries[0]
+    const languages = Object.values(country.languages)
+
+    return (
+      <>
+        <h1>{country.name.common}</h1>
+        <p>Capital {country.capital}</p>
+        <p>Area {country.area}</p>
+        <h2>Languages</h2>
+        <ul>
+          {languages.map(language =>
+            <li key={language}>{language}</li>
+          )}
+        </ul>
+        <img src={country.flags.png} alt={country.flags.alt} />
+      </>
+    )
+  }
+
+  return (
+    <p>Country not found</p>
+  )
+}
+
+export default Countries
